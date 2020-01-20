e820a8b4de6909af9049a4a007cea43f4cd6ff56
Luzaks <kanemekanik@gmail.com>
2020-01-20 15:18:12 -0600

    Add the game loop, console and the variables using for the physics of the game

--- main.cpp
+++ main.cpp
@@ -5,16 +5,26 @@ using namespace std;
 #include "olcConsoleGameEngine.h"
 
 
-class OneLoneCoder_Flappy-Bird {
+class OneLoneCoder_Flappy-Bird : public olcConsoleGameEngine {
 
 public:
+    OneLoneCoder_Flappy-Bird()
+    {
+        app_Name = L"Flappy Bird";
+    }
 
 private:
+    float bird_Position = 0.0f;
+    float bird_Velocity = 0.0f;
+    float bird_Acceleration = 0.0f;
 
 protected:
 
 };
 
 int main() {
+    OneLoneCoder_Flappy-Bird game;
     game.constructConsole(80, 48, 16, 16)
+
+    return 0;
 }
