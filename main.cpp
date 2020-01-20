c0fe6db474e550ee542a831064ef30ae16aed6f0
Luzaks <kanemekanik@gmail.com>
2020-01-20 15:45:46 -0600

    Add the drawing and part of the physics of the bird

--- main.cpp
+++ main.cpp
@@ -17,8 +17,46 @@ private:
     float bird_Position = 0.0f;
     float bird_Velocity = 0.0f;
     float bird_Acceleration = 0.0f;
+    float bird_Gravity = 100.0f;
 
 protected:
+    virtual bool OnUserCreate()
+    {
+                return true;
+    }
+    virtual bool OnUserUpdate(float elapsedTime)
+    {
+        if(m_keys[VK_SPACE].bPressed)
+        {
+            bird_Acceleration = 0.0f;
+            bird_Velocity = -bird_Gravity / 4.0f;
+        }
+        else
+        {
+            bird_Acceleration += bird_Gravity * elapsedTime;
+            if(bird_Acceleration >= bird_Gravity) bird_Acceleration = bird_Gravity;
+        }
+
+        bird_Velocity += bird_Acceleration * elapsedTime;
+        bird_Position += bird_Velocity * elapsedTime;
+
+        Fill (0, 0, ScreenWidth(), ScreenHeight(), L' ');
+
+        //The bird position in the screen.
+        int bird_X = (int)(ScreenWidth()/3.0f);
+
+        if (bird_Velocity > 0){
+            DrawString(bird_X, bird_Position + 0, L"\\\\\\");
+            DrawString(bird_X, bird_Position + 1, L"\\\\\\=Q");
+        }
+        else
+        {
+            DrawString(bird_X, bird_Position + 0, L"<///=Q");
+            DrawString(bird_X, bird_Position + 1, L"///");
+        }
+
+        return true;
+    }
 
 };
 
