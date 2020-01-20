c4db679ddbd53611b1bd0d0cbee9f7e6d5ddf96a
Luzaks <kanemekanik@gmail.com>
2020-01-20 15:13:15 -0600

    Add the main.cpp fiel with the basic structure of the game

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "olcConsoleGameEngine.h"
+
+
+class OneLoneCoder_Flappy-Bird {
+
+public:
+
+private:
+
+protected:
+
+};
+
+int main() {
+    game.constructConsole(80, 48, 16, 16)
+}
